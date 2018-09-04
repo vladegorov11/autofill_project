da5d0bac42a3b313f607ff7cfe2839c947b3a347
Влад Егоров <vladxd228@gmail.com>
2018-09-04 06:19:30 +0300

    fix

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,9 +1,15 @@
 module Api
   class ApiController < ActionController::API
 
-    def restrict_access
-        @projects = Project.find_by_api_token(params[:api_token])
-        head :unauthorized unless @projects
+
+    def verification_api_token 
+        @project = Project.find_by_api_token(params[:api_token])
+        head :unauthorized unless @project
+    end
+
+    def verification_auth_token 
+        @project = Project.find_by_auth_token(params[:auth_token])
+        head :unauthorized unless @project
     end
 
   end
--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -1,22 +1,27 @@
 module Api
   class GroupsController < ApiController
-    before_action :restrict_access
+    before_action :verification_auth_token, only: [:create]
+    before_action :verification_api_token, only: [:index, :show]
+    before_action :set_group 
 
     def index
-       @groups = @projects.groups
-       render json: { groups: @groups }, status: :ok
+      @groups = @project.groups
+      render json: { groups: @groups }, status: :ok
     end
 
     def show
-
+      render json: { group: [ @group, tags: @group.tags.pluck(:name)]}, status: :ok
     end
 
     def create
-
+      
+      render json: 
     end
 
     private
 
-
+    def set_group
+      @group = Group.find(params[:id])
+    end
   end
 end
