0607eb2b043d189b032fed4620893158e7358812
vladegorov11 <student3.oxygen.it.school@gmail.com>
2018-10-02 17:56:01 +0300

    add alert

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,12 +1,21 @@
 document.addEventListener('turbolinks:load', function () {
   $( ".link" ).click(function() {
-    const tag_id =  this.id
+    const tag_id =  this.id;
     $.post(
         `${window.location.pathname}/tags/destroy_tag`,
         {tag_id : tag_id},
         function(data) {
            $(`#${tag_id}d`).remove();
+           successAlert();
         }
     );
   });
 });
+
+function successAlert() {
+
+  $('#main').append('<div class="alert alert-success tests">delete tag success</div>')
+  setTimeout(function() {
+    $('.tests').remove();
+  }, 2000);
+}
